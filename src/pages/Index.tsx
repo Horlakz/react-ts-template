d7553f08f934698ba018f82a570e1be795beb66e
Horlakz <horlakz@protonmail.com>
2022-01-21 03:37:53 +0100

    added comments, page title, and changed content

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,28 @@
+
+// baselayout component
 import BaseLayout from "../layouts/BaseLayout";
 
 const Index = (): JSX.Element => {
+  // page title
+  document.title = "React TS Template";
+
   return (
     <BaseLayout>
+      {/* baselayout.(any) are optional */}
       <BaseLayout.Container>
         <BaseLayout.Header />
-        <div>this is just a template</div>
+        {/* your content goes here */}
+        <main className="p-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe veniam
+          accusamus voluptatibus corrupti eaque quam ad doloremque quia nulla
+          iste. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto
+          ducimus laudantium nesciunt voluptate repudiandae rerum voluptatibus
+          aspernatur accusantium suscipit fugit ea, ab qui odio cumque eos a,
+          mollitia pariatur minima ipsum similique deserunt incidunt est. Ullam
+          cumque dolore officia corporis sed possimus? Officia id rerum officiis
+          rem quod illum laboriosam vero, doloribus et accusamus tenetur veniam
+          porro. Corrupti, natus cumque.
+        </main>
         <BaseLayout.Footer />
       </BaseLayout.Container>
     </BaseLayout>
