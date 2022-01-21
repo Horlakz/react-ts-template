1c9d8e94a513aa2a1374ea11bbe005a249a8bf04
Horlakz <horlakz@protonmail.com>
2022-01-21 03:35:21 +0100

    added comments to the code

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,19 +1,25 @@
 import { FC } from 'react';
 
+// components
 import Header from "../components/BaseLayout/Header";
 import Footer from "../components/BaseLayout/Footer";
 import Container from "../components/BaseLayout/Container"
 
+
+// Props type
 interface Props {
     children: JSX.Element;
 }
 
 const BaseLayout: FC<Props> = ({ children }) => {
+  
+  // default layout for the application
   return (
-    <main>
+    <div>
       {children}
-    </main>
+    </div>
   );
 }
 
+// default export with the components
 export default Object.assign(BaseLayout, {Header, Footer, Container});
