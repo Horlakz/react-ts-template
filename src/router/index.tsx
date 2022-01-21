bf685c4241c0da3b94e1322ee15de536cdfa7f52
Horlakz <horlakz@protonmail.com>
2022-01-21 03:56:26 +0100

    added comments and error route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// home page
 import Index from "../pages/Index";
+
+// error page
+import Error from "../pages/error/Error";
 
 export default function Routere() {
   return (
     <Router>
       <Routes>
+        {/* error page */}
+        <Route path="*" element={<Error />} />
+
+        {/* home page */}
         <Route path="/" element={<Index />} />
       </Routes>
     </Router>
