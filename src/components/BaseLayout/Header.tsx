d9619babfc2ad529c6d1a179ff381395dec7c729
Horlakz <horlakz@protonmail.com>
2022-01-21 03:46:41 +0100

    added links to the nav menu

--- src/components/BaseLayout/Header.tsx
+++ src/components/BaseLayout/Header.tsx
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom"
+
 export default function Header() {
     return (
         <header className="justify-evenly flex border-b border-b-red-600 font-semibold p-2">
             <h1 className="text-lg text-red-600">React TypeScript + Sass + Tailwind Template</h1>
             <nav>
                 <ul className="flex space-x-4">
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact Us</li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to='/about'>About</Link></li>
+                    <li><Link to="/contact">Contact Us</Link></li>
                 </ul>
             </nav>
         </header>
