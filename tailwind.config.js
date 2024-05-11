cc13f76fc217abd013c20edbb1c02c267ff5c8ce
Horlakz <officialhorlakz@gmail.com>
2024-05-11 14:46:17 +0100

    refactor(tailwind config): updates success and info colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         primary: "#262626",
-        secondary: "#FFD700",
-        success: "#4BB543",
-        danger: "#FF4136",
-        warning: "#FFC300",
-        info: "#7FDBFF",
+        secondary: "#ffa500",
+        info: "#3498db",
+        danger: "#e74c3c",
+        success: "#2ecc71",
+        warning: "#f39c12",
       },
     },
   },
