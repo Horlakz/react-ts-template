65df8483ba7a275c81486552b695910ac24dc1cf
Horlakz <officialhorlakz@gmail.com>
2024-05-11 15:23:23 +0100

    refactor: Add ErrorBoundary to app.tsx for better error handling

--- src/app.tsx
+++ src/app.tsx
@@ -4,15 +4,19 @@ import "@/styles/globals.scss";
 
 import QueryClientProvider from "@/providers/queryclient.provider";
 import Router from "@/router";
+import { ErrorBoundary } from "react-error-boundary";
 import AppLayout from "./layout";
+import ServerErrorPage from "./pages/500";
 
 const App = () => {
   return (
     <AppLayout>
-      <QueryClientProvider>
-        <Router />
-        <Toaster />
-      </QueryClientProvider>
+      <ErrorBoundary FallbackComponent={ServerErrorPage}>
+        <QueryClientProvider>
+          <Router />
+          <Toaster />
+        </QueryClientProvider>
+      </ErrorBoundary>
     </AppLayout>
   );
 };
