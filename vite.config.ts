daef0195d2a1d557aa74329cb30002e072afe39a
Olakotan Daniel <officialhorlakz@gmail.com>
2025-04-11 00:43:29 +0100

    feat(vite): add Tailwind CSS plugin for enhanced styling capabilities

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
@@ -10,5 +11,5 @@ export default defineConfig({
     },
   },
 
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 });
